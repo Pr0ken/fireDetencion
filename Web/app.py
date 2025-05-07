eb73cff9eb756fac268b8259d8fdf74d8bf2eee8
Alejandro Navas <47699725+Pr0ken@users.noreply.github.com>
2025-05-07 10:03:31 +0200

    Add web

--- Web/app.py
+++ Web/app.py
@@ -1,23 +1,31 @@
 from flask import Flask, render_template, request
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
 import os
 from PIL import Image
 
 app = Flask(__name__)
+
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 # Cargar modelo
-modelo = load_model('modelo.h5')  # Ajusta el nombre si tu modelo es diferente
+modelo = load_model('fire_detection_model.keras')  
 
 # Función para predecir
 def predecir_imagen(ruta_imagen):
-    img = Image.open(ruta_imagen).resize((224, 224))  # Ajusta al tamaño que usa tu modelo
+    img = Image.open(ruta_imagen).resize((224, 224))
     img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
     prediccion = modelo.predict(img_array)
-    return "Incendio" if prediccion[0][0] > 0.5 else "No incendio"  # Ajusta si tienes más de una clase
+    prob_no_fire = prediccion[0][0]
+    print(f"Probabilidad no incendio: {prob_no_fire:.4f}")
+    if prob_no_fire > 0.5:
+        return f"No incendio ({prob_no_fire:.2%})"
+    else:
+        return f"Incendio ({1 - prob_no_fire:.2%})"
+    
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
